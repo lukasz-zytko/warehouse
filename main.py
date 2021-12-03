1e02e4991f41d7df3e1c729214985e5445ab9486
Lukasz Zytko <zytko.lukasz@gmail.com>
2021-12-03 20:35:06 +0100

    Add corrections

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def menu(arg):
-    if arg == "exit":
-        print("OK. See you later..")
-        exit(1)
-    else:
-        pass
-
-choice = input("Hello. What do you want to do?:")
-
+menu = input("Hello. What do you want to do? [exit]:")
 if __name__ == "__main__":
-    while choice != "exit":
-        menu(choice)
-        choice = input("What do you want to do?:")
+    while menu != "exit":
+        menu = input("What do you want to do?:")
+    print("Ok. See you later..")
+    
