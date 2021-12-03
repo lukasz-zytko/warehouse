9abc39161843b8a7c36f1b04265233411d134797
Lukasz Zytko <zytko.lukasz@gmail.com>
2021-12-03 20:45:37 +0100

    Add corrections

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+items = ["Ron Barcelo", "Cuban cigars", "Chocolate sweets", "Red wine"]
+
+def show():
+    print(items)
+
 menu = input("Hello. What do you want to do? [exit]:")
 if __name__ == "__main__":
     while menu != "exit":
+        if menu == "show":
+            show()
         menu = input("What do you want to do?:")
     print("Ok. See you later..")
     
